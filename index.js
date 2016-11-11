4ac410f3a1f86d2d5ae6b2c68b8815bd5df61f3c
Chris Angelico <rosuav@gmail.com>
2016-11-12 07:29:18 +1100

    Implement a generator-based version of the same code

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ var pg = require('pg');
 var pgconfig = require('./secret.js');
 var client = new pg.Client(pgconfig);
 
-//Step 1: Connect. (In real-world work, this would be done just once for the app.)
-client.connect(function (err) {
-	if (err) throw err;
+function run(gen) {
+	function advance(err, result) {
+		var yielded = err ? gen.throw(err) : gen.next(result);
+		if (yielded.done) return; //Generator has returned.
+		//TODO: Handle a yielded promise
+	}
+	gen = gen(advance); //Invoke the generator (we don't need the function)
+	advance();
+}
+
+//And here's how asynchronous code looks.
+run(function*(next) {
+	//Step 1: Connect. (In real-world work, this would be done just once for the app.)
+	yield client.connect(next);
 
 	//Step 2: Submit a query.
-	client.query('select value1, value2 from demo where id = $1', [3], function (err, result) {
-		if (err) throw err;
- 
-		//Print that to the console (the real guts of the program)
-		console.log(result.rows[0]);
+	result = yield client.query('select value1, value2 from demo where id = $1', [3], next);
+	
+	//Print that to the console (the real guts of the program)
+	console.log(result.rows[0]);
 
-		//Step 3: Disconnect
-		client.end(function (err) {
-			if (err) throw err;
-			//And then we backtab all the way.
-		});
-	});
+	//Step 3: Disconnect
+	yield client.end(next)
+	//And no backtabbing (or backstabbing).
 });
