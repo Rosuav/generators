c7415bec7a8269cbef231bda300bc16d09258598
Chris Angelico <rosuav@gmail.com>
2016-11-15 11:09:18 +1100

    Oops - remove 'next' calls

--- synchronous.js
+++ synchronous.js
@@ -5,16 +5,16 @@ var client = new pg.Client(pgconfig);
 //Theoretically, this could be a synchronous function.
 //It wouldn't actually work this way, though, because pg.Client does not
 //support synchronous calls.
-(function*(next) {
+(function() {
 	//Step 1: Connect. (In real-world work, this would be done just once for the app.)
-	client.connect(next);
+	client.connect();
 
 	//Step 2: Submit a query.
-	result = client.query('select value1, value2 from demo where id = $1', [3], next);
+	result = client.query('select value1, value2 from demo where id = $1', [3]);
 	
 	//Print that to the console (the real guts of the program)
 	console.log(result.rows[0]);
 
 	//Step 3: Disconnect
-	client.end(next);
+	client.end();
 })();
