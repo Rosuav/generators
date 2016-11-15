6fc7bb79a3f53ff1662cf0d50466800c12efb0ca
Chris Angelico <rosuav@gmail.com>
2016-11-15 11:11:14 +1100

    Synchronize callback-hell on the others by putting it in an IIFE

--- callback-hell.js
+++ callback-hell.js
@@ -4,23 +4,25 @@ var pg = require('pg');
 var pgconfig = require('./secret.js');
 var client = new pg.Client(pgconfig);
 
-//Step 1: Connect. (In real-world work, this would be done just once for the app.)
-client.connect(function (err) {
-	//Don't forget to check for err everywhere. If you forget, your editor MAY
-	//notice this and warn you, but otherwise, it'll silently swallow errors.
-	if (err) throw err;
-
-	//Step 2: Submit a query.
-	client.query('select value1, value2 from demo where id = $1', [3], function (err, result) {
+(function() {
+	//Step 1: Connect. (In real-world work, this would be done just once for the app.)
+	client.connect(function (err) {
+		//Don't forget to check for err everywhere. If you forget, your editor MAY
+		//notice this and warn you, but otherwise, it'll silently swallow errors.
 		if (err) throw err;
- 
-		//Print that to the console (the real guts of the program)
-		console.log(result.rows[0]);
 
-		//Step 3: Disconnect
-		client.end(function (err) {
+		//Step 2: Submit a query.
+		client.query('select value1, value2 from demo where id = $1', [3], function (err, result) {
 			if (err) throw err;
-			//And then we backtab all the way.
+	 
+			//Print that to the console (the real guts of the program)
+			console.log(result.rows[0]);
+
+			//Step 3: Disconnect
+			client.end(function (err) {
+				if (err) throw err;
+				//And then we backtab all the way.
+			});
 		});
 	});
-});
+})();
