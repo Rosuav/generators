960f7ffd395d29960ecc21997ade0a5144033485
Chris Angelico <rosuav@gmail.com>
2017-03-02 21:21:58 +1100

    Add a new (broken) promise-based example

---
+++ 03promises.js
@@ -0,0 +1,23 @@
+//Promises. Basically "callbacks with less indentation".
+//Not everything supports them, though, and this example actually won't
+//work, because client.connect() doesn't return a promise.
+
+var pg = require('pg');
+var pgconfig = require('./secret.js');
+var client = new pg.Client(pgconfig);
+
+(function() {
+	//Step 1: Connect. (In real-world work, this would be done just once for the app.)
+	client.connect().then(function (err) {
+		//Step 2: Submit a query.
+		return client.query('select value1, value2 from demo where id = $1', [3]);
+	}).then(function (result) {
+		//Print that to the console (the real guts of the program)
+		console.log(result.rows[0]);
+
+		//Step 3: Disconnect
+		return client.end();
+	}).catch(function (err) {
+		console.error(err);
+	});
+})();
